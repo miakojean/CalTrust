0cecf67213cb6a2bd15e7866f50c1be638e818ca
miakojean <miakojeanyves@gmail.com>
2025-07-03 19:46:36 +0000

    settings about css reseting

--- CalTrustFront/src/App.vue
+++ CalTrustFront/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Bienvenu</h1>
+  <h1>Bienvenu Chez vous</h1>
 </template>
 
 <script>
@@ -8,6 +8,6 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped>
 
 </style>
