9ff11675f6206ac58870cdbdf38a97e69e3370b1
miakojean <miakojeanyves@gmail.com>
2025-07-07 03:19:39 +0000

    07/07/2025

---
+++ CalTrustFront/src/section/ctaSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="main__section">
+        <div class="main__container">
+            <h1>
+                Votre avis compte, faites-le nous savoir!
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Pariatur impedit eaque 
+                ipsa culpa recusandae temporibus, non 
+                cumque nostrum laborum quibusdam?
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- CalTrustFront/src/views/HomeView.vue
+++ CalTrustFront/src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
     <navbar/>
+    <ctaSection></ctaSection>
   </main>
 </template>
 
 <script>
 import navbar from '@/layout/navbar.vue';
+import ctaSection from '@/section/ctaSection.vue';
 export default {
     components:{
-        navbar,
+      navbar,
+      ctaSection
     },
 
     setup(){
