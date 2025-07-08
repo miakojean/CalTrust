190dd2e580f72869bc070d1aadd8589a9069240d
miakojean <miakojeanyves@gmail.com>
2025-07-08 16:51:36 +0000

    first day of challenge (2)

--- CalTrustFront/src/section/featureSection.vue
+++ CalTrustFront/src/section/featureSection.vue
@@ -1,15 +1,64 @@
 <template>
   <section class="main__section">
     <div class="main__container">
-        <h2>
-            Donnez votre avis, Aidez les autres à choisir
-        </h2>
+      <h2>
+        Donnez votre avis, Aidez les autres à choisir
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Pariatur impedit eaque 
+        ipsa culpa recusandae temporibus, non 
+        cumque nostrum laborum quibusdam?
+      </p>
+    </div>
+    <div class="features__container">
+      <div class="features__cards">
+        <span>
+          <i class="ri-delete-bin-6-line"> </i>
+        </span>
+        <h3 class="features__title">Spam report</h3>
         <p>
-            Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Pariatur impedit eaque 
-            ipsa culpa recusandae temporibus, non 
-            cumque nostrum laborum quibusdam?
+          Lorem ipsum dolor sit, amet consectetur 
+          adipisicing elit. Laudantium maxime 
+          consectetur sapiente.
         </p>
+      </div>
+
+      <div class="features__cards">
+        <span>
+          <i class="ri-delete-bin-6-line"> </i>
+        </span>
+        <h3 class="features__title">Spam report</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur 
+          adipisicing elit. Laudantium maxime 
+          consectetur sapiente.
+        </p>
+      </div>
+
+      <div class="features__cards">
+        <span>
+          <i class="ri-delete-bin-6-line"> </i>
+        </span>
+        <h3 class="features__title">Spam report</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur 
+          adipisicing elit. Laudantium maxime 
+          consectetur sapiente.
+        </p>
+      </div>
+
+      <div class="features__cards">
+        <span>
+          <i class="ri-delete-bin-6-line"> </i>
+        </span>
+        <h3 class="features__title">Spam report</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur 
+          adipisicing elit. Laudantium maxime 
+          consectetur sapiente.
+        </p>
+      </div>
     </div>
   </section>
 </template>
@@ -21,5 +70,9 @@ export default {
 </script>
 
 <style>
-
+.features__container{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
