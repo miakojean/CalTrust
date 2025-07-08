c89802af317ed25db80021946903cbda978bb269
miakojean <miakojeanyves@gmail.com>
2025-07-08 14:47:47 +0000

    cta section done (1/3)

---
+++ CalTrustFront/src/components/tools/researchBox.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="research__box">
+        <label for="firms">Je cherche des avis sur</label>
+        <div class="input__family">
+            <input type="text" id="firms" placeholder="Trouver un service, une entreprise, un produits">
+            <button class="research__button">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+.research__box{
+    width: 100%;
+    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    border-radius: 1rem;
+}
+
+.input__family{
+    width: 100%;
+    display: flex;
+    gap: 1rem;
+}
+
+
+
+.research__button{
+    padding: 1rem;
+    border-radius: 50%;
+    height: 100%;
+    background: #358cd3;
+    color: #fff;
+    font-size: 1.1rem;
+    transition: ease-in-out 1.2s;
+}
+
+.research__button:hover{
+    background: #114977;
+    color: #fff;
+    transition: ease-in-out 0.8s;
+}
+
+</style>
--- CalTrustFront/src/section/ctaSection.vue
+++ CalTrustFront/src/section/ctaSection.vue
@@ -2,7 +2,7 @@
     <section class="main__section">
         <div class="main__container">
             <h1>
-                We're changing the way people connect
+                Donnez votre avis, Aidez les autres à choisir
             </h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur 
@@ -10,10 +10,7 @@
                 ipsa culpa recusandae temporibus, non 
                 cumque nostrum laborum quibusdam?
             </p>
-            <div class="btn__flex">
-                <main-button></main-button>
-                <more-button/>
-            </div>
+            <research-box/>
         </div>
         <div class="pic__container">
             <img class="pair" src="@/assets/Pictures/picture_seven.jpg" alt="Reunion of people" />
@@ -27,9 +24,10 @@
 <script>
 import mainButton from '@/components/button/mainButton.vue';
 import moreButton from '@/components/button/moreButton.vue';
+import researchBox from '@/components/tools/researchBox.vue';
 export default {
     components:{
-        mainButton, moreButton
+        mainButton, moreButton, researchBox
     }
 }
 </script>
@@ -59,15 +57,6 @@ export default {
         gap: 0.5rem;
     }
 
-    .main__container{
-        padding-top: 2rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 1rem;
-    }
-
     .pic__container {
         min-height: 50vh;
         display: flex;
@@ -82,9 +71,40 @@ export default {
     .pic__container img:nth-child(even) {
         align-self: flex-end; /* Pousse vers le bas */
     }
+}
+
+@media (min-width: 766px) {
+    .pic__container {
+        min-height: 50vh;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: flex-start; /* Alignement en haut par défaut */
+        gap: 0.5rem;
+        height: auto;
+    }
+
+        /* Cible les images paires (2e, 4e) pour les aligner en bas */
+    .pic__container img:nth-child(even) {
+        align-self: flex-end; /* Pousse vers le bas */
+    }
 
     .pair{
         margin-top: 2rem;
+    }
+}
+
+@media (min-width: 1024px){
+
+    .main__container {
+        width: 50%;
+    }
+    .pic__container {
+        width: 50%;
+    }
+
+    .pic__container img:nth-child(even) {
+        align-self: flex-end; /* Pousse vers le bas */
     }
 }
 </style>
