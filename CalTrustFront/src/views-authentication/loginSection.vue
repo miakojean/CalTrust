abc4d1163dc4d1a2f9e9faf9c4284516f2dffb4e
miakojean <miakojeanyves@gmail.com>
2025-07-15 23:36:00 +0000

    reorfering form

--- CalTrustFront/src/views-authentication/loginSection.vue
+++ CalTrustFront/src/views-authentication/loginSection.vue
@@ -5,11 +5,11 @@
       <ourForm/>
     </div>
     <div class="pic__container">
-      <img class="pair" src="@/assets/Pictures/picture_seven.jpg" alt="Reunion of people" />
-      <img class="impair" src="@/assets/Pictures/picture_six.jpg" alt="Reunion of people" />
-      <img class="pair" src="@/assets/Pictures/picture_one.jpg" alt="Reunion of people" />
-      <img class="impair" src="@/assets/Pictures/picture_four.jpg" alt="Reunion of people" /> 
-    </div>
+        <img class="pair" src="@/assets/Pictures/picture_seven.jpg" alt="Reunion of people" />
+        <img class="impair" src="@/assets/Pictures/picture_six.jpg" alt="Reunion of people" />
+        <img class="pair" src="@/assets/Pictures/picture_one.jpg" alt="Reunion of people" />
+        <img class="impair" src="@/assets/Pictures/picture_four.jpg" alt="Reunion of people" /> 
+        </div>
   </section>
 </template>
 
@@ -23,12 +23,36 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .login__section{
   min-height: 100vh;
+  width: 100%;
+}
+
+.main__container{
+  width: 100%;
 }
 
 .pic__container{
   display: none;
+}
+
+@media (min-width: 1024px) {
+
+  .login__section{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: 2rem;
+  }
+
+  .main__container{
+    width: 45%;
+  }
+
+  .pic__container{
+    display: flex;
+    width: 45%;
+  }
 }
 </style>
