5fa764254b9903978a6da30bae9990c4d398aa08
miakojean <miakojeanyves@gmail.com>
2025-08-09 17:58:38 +0000

    travaille sur les informations

--- CalTrustFront/src/views-companies/details/companyDetails.vue
+++ CalTrustFront/src/views-companies/details/companyDetails.vue
@@ -3,10 +3,19 @@
     <new-navbar/>
     <section class="company__section">
       <div class="company__left-sidebar">
-        <companyDetailSection/>
+        <companyDetailCard
+          :firm="firm.company_name"
+          :category="firm.category"
+          :user="firm.id"
+          :email="firm.email"
+          :website="firm.website"
+          :addresse="firm.address"
+        />
       </div>
       <div class="company__center">
-        <companyReviewDetails/>
+        <companyReviewDetails
+  
+        />
       </div>
       <div class="company__right-sidebar">
         <globalRatingCard/>
@@ -21,10 +30,12 @@ import newNavbar from '@/layout/newNavbar.vue';
 import companyDetailSection from './companyDetailSection.vue';
 import companyReviews from '../companyReviews.vue';
 import globalRatingCard from '@/components/cards/globalRatingCard.vue';
+import companyDetailCard from './companyDetailCard.vue';
 import companyReviewDetails from './companyReviewDetails.vue';
 import footerSection from '@/layout/footerSection.vue';
 import { fetchRecentsFirms } from '../_companyservices';
 import { ref, onMounted } from 'vue';
+import CompanyDetailCard from './companyDetailCard.vue';
 
 export default {
 
@@ -33,27 +44,49 @@ export default {
     footerSection,
     companyReviews,
     companyDetailSection,
+    companyDetailCard,
     companyReviewDetails,
     globalRatingCard
   },
 
   setup(){
     
-    const firm = ref({})
+    const firm = ref({
+      name:"",
+      company_name:"",
+      address: "",
+      email:"",
+      website:"",
+      category:""
+    })
 
     const firmId = history.state.id;
 
-    onMounted( async ( ) => { 
+    onMounted(async () => { 
       try {
         const response = await fetchRecentsFirms(firmId);
+
         if (response) {
-          firm.value = response.data; // Stockez les données
-          console.log('Entreprise récente chargée:', firm.value);
+          // On récupère le bloc "name"
+          const nameData = response.data.name;
+          const globalResponse = response.data
+
+          firm.value = {
+            id: nameData.user_id,
+            company_name: nameData.company_name,
+            address: nameData.address,
+            email: nameData.email,        // Ajoutez
+            website: globalResponse.website,     // Ajoutez
+            category: globalResponse.category_display   // Ajoutez si nécessaire
+          };
+
+          console.log('Entreprise chargée:', firm.value);
         }
       } catch (error) {
-          console.error("Erreur de chargement:", error);
+        console.error("Erreur de chargement:", error);
       }
     });
+
 
     return {firm, firmId}
   }
